99547541479f08b393e14c08d9d120bbd4e4dff0
CarlosEscobar <caeclina@hotmail.com>
2024-07-10 19:14:54 -0500

    first commit

---
+++ tipos-de-datos.py
@@ -0,0 +1,14 @@
+"string" #para textos
+'string'
+"""string"""
+'''string'''
+"""tis datos son
+    nombre: Carlos
+    apellido: Escobar"""
+# Numerico
+15#Entero
+2.5#Flotante
+
+#booleano
+True#Verdadero
+False#Falso
