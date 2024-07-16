beeb7f180c27a25488912b90e460ec4f33b9570e
CarlosEscobar <caeclina@hotmail.com>
2024-07-15 19:47:49 -0500

    Se abordaron fundamentos de pythom

---
+++ fundamentos.py
@@ -0,0 +1,16 @@
+from os import system
+system("clear")
+name="   Carlos  "
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[0:2])
+last_name = "Escobar Cordoba"
+print(name * 5)
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name.strip() + " " + last_name.strip()
+      )
+
---
+++ variables/variables.py
@@ -0,0 +1,12 @@
+Texto1 = "Colombia"
+Texto2 = " vs "
+Texto3 = "Uruguay"
+Texto4 = " hoy ganamos"
+final = Texto1 + Texto2 + Texto3 + Texto4
+print(final)
+
+#CamelCase
+nombreCompletoDeTuProfe = "Carlos-Escobar"
+
+#SnakeCase es la que debemos utilizar en Python
+nombre_completo_de_tu_profe = "Carlos-Escobar"
