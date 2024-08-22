f1d6060e020a71a00281ab59456f067a46befb8f
CarlosEscobar <caeclina@hotmail.com>
2024-08-21 19:31:53 -0500

    abordamos input

---
+++ inputs/inputs.py
@@ -0,0 +1,6 @@
+from os import system
+system("clear")
+#Pedir un dato al usuario
+nombre = input("Cual es tu nombre")
+print(f'el nombre es {nombre}')
+print(type(nombre))
---
+++ inputs/numeros.py
@@ -0,0 +1,6 @@
+from os import system
+system("clear")
+#Pedir un dato un numero
+numero = input("dame un numero")
+resultado=float(numero)*2 
+print(resultado)
