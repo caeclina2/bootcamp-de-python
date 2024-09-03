9fde61240cf34e03020689251b1bd6ba38546c89
CarlosEscobar <caeclina@hotmail.com>
2024-09-02 20:03:18 -0500

    Uso de funciones

---
+++ uso-de-funciones.py
@@ -0,0 +1,9 @@
+from os import system
+system("clear")
+def caeclina(name,lastname=""):
+    print("Hola",name,lastname)
+caeclina("Carlos","Escobar")
+caeclina("Alberto")
+caeclina(lastname="Escobar",name="carlos")
+   
+          
